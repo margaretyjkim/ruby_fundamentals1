c00e038d576724d22aaccf9dd98bf1e42cb039c7
margaretyjkim <margaret.yj.kim@gmail.com>
2015-10-27 13:43:58 -0400

    exercise1

---
+++ exercise2.rb
@@ -0,0 +1,12 @@
+tip = 55 * (0.20)
+puts "A good tip might be #{tip} dollars for a 55 dollar meal."
+
+meal_time = "when do you want to eat?"
+time = 2.to_s
+
+puts meal_time + time
+
+output = 45628 * 7839
+puts "The answer is #{output}"
+
+puts (true && false) || (false && true) || !(false && false)
