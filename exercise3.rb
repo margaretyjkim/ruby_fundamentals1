ddf26371afc6db8bc9f17c7a07d12306925f8990
margaretyjkim <margaret.yj.kim@gmail.com>
2015-10-27 14:57:57 -0400

    exercise4

--- exercise3.rb
+++ exercise3.rb
@@ -4,4 +4,8 @@ puts "Hi #{name}!"
 
 puts "How old are you?"
 age = gets.chomp.to_i
-puts"I am #{age} years old."
+puts"You are #{age} years old."
+
+current_year = 2015
+birth_year = current_year - age
+puts "That means you were born in #{birth_year}!"
---
+++ exercise4.rb
@@ -0,0 +1,14 @@
+number = (1..100)
+number.each do |number|
+
+if number %3 == 0	&& number %5 == 0
+	puts "BitMaker"
+
+elsif number %3 == 0
+	puts "Bit "
+elsif number %5 == 0
+	puts "Maker "
+else
+	puts number
+end
+end
